098eb3643a1292f3b0b213c3518d0328acaffd23
Allison Sadin <aesadin@gmail.com>
2020-07-15 11:14:51 -0700

    expected failing test
    Co-authored-by: James Henager <jameschenager@gmail.com>
    Co-authored-by: Cody Fritz <clanalia55@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,13 +1,31 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
-using System;
-using System.Collections.Generic;
 
-namespace ToDoList.TestTools
+namespace ToDoList.Tests
 {
   [TestClass]
   public class ItemTests
   {
 
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      
+      //act
+      string result = newItem.Description;
+      
+      //assert
+      Assert.AreEqual(description, result);
+    }
   }
 }
