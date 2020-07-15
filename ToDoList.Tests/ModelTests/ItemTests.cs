230e91312756305880ba092a00c2e53ba3500548
Allison Sadin <aesadin@gmail.com>
2020-07-15 10:40:52 -0700

    initialize all files for program
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>
    Co-authored-by: Cody Fritz <clanalia55@gmail.com>
    Co-authored-by: James Henager <jameschenager@gmail.com>

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ToDoList.TestTools
+{
+  [TestClass]
+  public class ItemTests
+  {
+
+  }
+}
