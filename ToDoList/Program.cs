104865f039635c167bbaffe25eeb1dbc53aa3ea9
Allison Sadin <aesadin@gmail.com>
2020-07-15 15:27:30 -0700

    creates user functionality that allows for adding to item list and viewing item list
    Co-authored-by: James Henager <jameschenager@gmail.com>
    Co-authored-by: Cody Fritz <clanalia55@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

---
+++ ToDoList/Program.cs
@@ -0,0 +1,38 @@
+using ToDoList;
+using ToDoList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToDoList
+{
+  class ToDoList
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to the To Do List");
+      Console.WriteLine("Would you like to add an item to your list or view your list? Add/View");
+      string userResponse = Console.ReadLine();
+
+      if (userResponse.ToUpper() == "ADD")
+      {
+        Console.WriteLine("Enter the description of your to-do item: ");
+        Item newItem = new Item(Console.ReadLine());
+      }
+      else if (userResponse.ToUpper() == "VIEW")
+      {
+      List<Item> viewStuff = (Item.GetAll());
+      
+      foreach (Item thisItem in viewStuff)
+      {
+        Console.WriteLine(thisItem.Description);
+      }
+      }
+      else
+      {
+        Console.WriteLine("Please enter either 'Add' or 'View'");
+      }
+      Main();
+    }
+  }
+}
