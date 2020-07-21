df315fe8a11a2f2ae5cf47970e935091fb6cc9eb
Allison Sadin <aesadin@gmail.com>
2020-07-21 11:57:26 -0700

    added new functionality to program as well as view method to make it interactive on a webpage

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,19 +1,16 @@
 using System.Collections.Generic;
-using System;
 
 namespace ToDoList.Models
 {
   public class Item
   {
-
     public string Description { get; set; }
     private static List<Item> _instances = new List<Item> {};
 
-    public Item(string description)
+    public Item (string description)
     {
       Description = description;
       _instances.Add(this);
-      
     }
 
     public static List<Item> GetAll()
@@ -25,5 +22,6 @@ namespace ToDoList.Models
     {
       _instances.Clear();
     }
+
   }
 }
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,43 +1,20 @@
-using ToDoList;
-using ToDoList.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ToDoList
 {
-  class ToDoList
+  public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to the To Do List");
-      Start();
-    }
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-    public static void Start()
-    {
-      Console.WriteLine("Would you like to add an item to your list or view your list? Add/View");
-      string userResponse = Console.ReadLine();
-
-      if (userResponse.ToUpper() == "ADD")
-      {
-        Console.WriteLine("Enter the description of your to-do item: ");
-        Item newItem = new Item(Console.ReadLine());
-      }
-      else if (userResponse.ToUpper() == "VIEW")
-      {
-      List<Item> viewStuff = (Item.GetAll());
-      
-      foreach (Item thisItem in viewStuff)
-      {
-        Console.WriteLine(thisItem.Description);
-      }
-      }
-      else
-      {
-        Console.WriteLine("Please enter either 'Add' or 'View'");
-      }
-      Start();
+      host.Run();
     }
   }
 }
